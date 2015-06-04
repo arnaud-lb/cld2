26c108a5780958b01f3f58617c84ff637e34b95b
Arnaud Le Blanc <arnaud.lb@gmail.com>
2015-06-04 17:14:53 +0200

    Added DetectSummary()

--- cld2.cc
+++ cld2.cc
@@ -39,3 +39,44 @@ const char* DetectLang(char *data, int length) {
 
     return CLD2::LanguageCode(summary_lang);
 }
+
+const char* DetectLangSummary(char *data, int length, int is_plain_text, int allow_extended_lang, const char *tld_hint, const char *language_hint, const char **language3, int *percent3, int *text_bytes, int *is_reliable) {
+
+    CLD2::CLDHints cldhints = {NULL, NULL, 0, CLD2::UNKNOWN_LANGUAGE};
+    int flags = 0;
+    CLD2::Language cld_language3[3];
+    double normalized_score3[3];
+    CLD2::ResultChunkVector resultchunkvector;
+    bool cld_is_reliable;
+    int i;
+
+    if (length <= 0) {
+        length = strlen(data);
+    }
+
+    if (language_hint) {
+        cldhints.language_hint = CLD2::GetLanguageFromName(language_hint);
+    }
+
+    CLD2::Language summary_lang = CLD2::UNKNOWN_LANGUAGE;
+
+    summary_lang = CLD2::ExtDetectLanguageSummary(data, 
+            length,
+            (bool) is_plain_text,
+            &cldhints,
+            flags,
+            cld_language3,
+            percent3,
+            normalized_score3,
+            &resultchunkvector,
+            text_bytes,
+            &cld_is_reliable);
+
+    for (i = 0; i < 3; i++) {
+        language3[i] = CLD2::LanguageCode(cld_language3[i]);
+    }
+
+    *is_reliable = cld_is_reliable;
+
+    return CLD2::LanguageCode(summary_lang);
+}
--- cld2.go
+++ cld2.go
@@ -11,6 +11,31 @@ package cld2
 import "C"
 import "unsafe"
 
+type Results struct {
+	Language   string
+	Summary    [3]Result
+	TextBytes  int
+	IsReliable bool
+}
+
+type Result struct {
+	Language string
+	Percent  int
+}
+
+type Hints struct {
+	tld      string
+	language string
+}
+
+func (h *Hints) SetTLD(tld string) {
+	h.tld = tld
+}
+
+func (h *Hints) SetLanguageString(language string) {
+	h.language = language
+}
+
 // Detect returns the language code for detected language
 // in the given text.
 func Detect(text string) string {
@@ -22,4 +47,67 @@ func Detect(text string) string {
 		lang = C.GoString(res)
 	}
 	return lang
+}
+
+func DetectSummary(text string, isPlainText bool, allowExtendedLanguages bool, hints *Hints) *Results {
+
+	cText := C.CString(text)
+	defer C.free(unsafe.Pointer(cText))
+
+	var cTLDHint, cLanguageHint *C.char
+
+	if hints.tld != "" {
+		cTLDHint = C.CString(hints.tld)
+		defer C.free(unsafe.Pointer(cTLDHint))
+	}
+
+	if hints.language != "" {
+		cLanguageHint = C.CString(hints.language)
+		defer C.free(unsafe.Pointer(cLanguageHint))
+	}
+
+	var language3 [3]*C.char
+	var percent3 [3]C.int
+	var textBytes C.int
+	var isReliable C.int
+
+	summaryLanguage := C.DetectLangSummary(
+		cText,
+		C.int(len(text)),
+		boolToCInt(isPlainText),
+		boolToCInt(allowExtendedLanguages),
+		cTLDHint,
+		cLanguageHint,
+		(**C.char)(unsafe.Pointer(&language3[0])),
+		(*C.int)(unsafe.Pointer(&percent3[0])),
+		&textBytes,
+		&isReliable,
+	)
+
+	results := &Results{
+		Language:   C.GoString(summaryLanguage),
+		IsReliable: cIntToBool(isReliable),
+		TextBytes:  int(textBytes),
+	}
+
+	for i := 0; i < 3; i++ {
+		results.Summary[i].Language = C.GoString(language3[i])
+		results.Summary[i].Percent = int(percent3[i])
+	}
+
+	return results
+}
+
+func boolToCInt(b bool) C.int {
+	if b {
+		return 0
+	}
+	return 1
+}
+
+func cIntToBool(i C.int) bool {
+	if i == 0 {
+		return false
+	}
+	return true
 }
